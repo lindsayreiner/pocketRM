8f030e1ac5bc3d197b34267839e77f985221adcc
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-11 21:36:37 -0500

    updating logo and images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,48 +1,65 @@
-import React from 'react';
+import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import { Button } from 'react-bootstrap'
+import { Button } from "react-bootstrap";
+import pocketRMLogo from "../images/pocketrm-logo.png";
 
-import 'normalize.css';
-import '../styles/App.css'
-
-
+import "normalize.css";
+import "../styles/App.css";
 
 export default function Navbar() {
-    return (
-        <>
-
-            <nav className="navbar">
-                <Link className="bizName" to="/"><b>PocketRM</b></Link>
-                <input type="checkbox" id="navToggle" className="navToggle" />
-                <ul className="navBtnCont">
-
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/dashboard">Dashboard</NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/contacts">Contacts</NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/calendar">Calendar</NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/reminders" >Reminders</NavLink>
-                    </li>
-                </ul>
-                <label for="navToggle" className="navToggleLabel">
-                    <span></span>
-                </label>
-                <div className="acctBtns container">
-                    <section>
-                        <Link to="/login"><Button inverted color='violet'>
-                            Login
-                        </Button></Link>
-                        <Link to="/register"> <Button inverted color='violet'>
-                            Sign up
-                        </Button></Link>
-                    </section>
-                </div>
-            </nav >
-        </ >
-    )
+  return (
+    <>
+      <nav className="navbar">
+        {/* <Link className="bizName" to="/"> */}
+        <img
+          id="nav-logo"
+          src={pocketRMLogo}
+          alt="PocketRM Logo"
+          height="60px"
+        />
+        {/* </Link> */}
+        <input type="checkbox" id="navToggle" className="navToggle" />
+        <ul className="navBtnCont">
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/dashboard">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/contacts">
+              Contacts
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/calendar">
+              Calendar
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/reminders">
+              Reminders
+            </NavLink>
+          </li>
+        </ul>
+        <label for="navToggle" className="navToggleLabel">
+          <span></span>
+        </label>
+        <div className="acctBtns container">
+          <section>
+            <Link to="/login">
+              <Button inverted color="violet">
+                Login
+              </Button>
+            </Link>
+            <Link to="/register">
+              {" "}
+              <Button inverted color="violet">
+                Sign up
+              </Button>
+            </Link>
+          </section>
+        </div>
+      </nav>
+    </>
+  );
 }
