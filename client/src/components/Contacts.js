7951e0510cc3afec769741fd30de8bea33f7c425
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2022-01-13 16:06:46 -0500

    committing to do a pull

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
 
 export default function Contacts() {
     return (
         <div>
-            <h1>Contacts</h1>
+            <Image></Image>
         </div>
     )
 }
