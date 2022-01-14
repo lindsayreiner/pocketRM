9d6e8731c267b774ca69724f31e82b92dd719b18
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-13 19:00:55 -0500

    making header sticky and changed size of login btn

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,58 +7,57 @@ import "normalize.css";
 import "../styles/App.css";
 
 export default function Navbar() {
-    return (
-        <>
-            <nav className="navbar">
-                <Link to="/">
-                    <img
-                        className="navLogo"
-                        src={pocketRMLogo}
-                        alt="PocketRM Logo"
-                        height="35px"
-                    />
-                </Link>
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/">
+          <img
+            className="navLogo"
+            src={pocketRMLogo}
+            alt="PocketRM Logo"
+            height="40px"
+          />
+        </Link>
 
-                <input type="checkbox" id="navToggle" className="navToggle" />
+        <input type="checkbox" id="navToggle" className="navToggle" />
 
-                <ul className="navBtnCont">
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/dashboard">
-                            Dashboard
-                        </NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/contacts">
-                            Contacts
-                        </NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/calendar">
-                            Calendar
-                        </NavLink>
-                    </li>
-                    <li className="navBtn">
-                        <NavLink className="navBtnLink" to="/reminders">
-                            Reminders
-                        </NavLink>
-                    </li>
-                </ul>
+        <ul className="navBtnCont">
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/dashboard">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/contacts">
+              Contacts
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/calendar">
+              Calendar
+            </NavLink>
+          </li>
+          <li className="navBtn">
+            <NavLink className="navBtnLink" to="/reminders">
+              Reminders
+            </NavLink>
+          </li>
+        </ul>
 
-                <label htmlFor="navToggle" className="navToggleLabel">
-                    <span></span>
-                </label>
+        <label htmlFor="navToggle" className="navToggleLabel">
+          <span></span>
+        </label>
 
-                <div className="acctBtns">
-
-                    <Link to="/login"><Button >
-                        Login
-                    </Button></Link>
-                    <Link to="/register"> <Button>
-                        Sign up
-                    </Button></Link>
-                </div>
-            </nav>
-
-        </>
-    );
+        <div className="acctBtns">
+          <Link to="/login">
+            <Button id="login-button">Login</Button>
+          </Link>
+          <Link to="/register">
+            {" "}
+            <Button>Sign up</Button>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
