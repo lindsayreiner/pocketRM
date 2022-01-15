c9006735ee9ff6a8f51201f4382280b5d75ce615
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2022-01-14 20:06:52 -0500

    added user seeds, updated user model and schema to remove username

--- server/models/User.js
+++ server/models/User.js
@@ -2,12 +2,6 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
   name: {
     type: String,
     required: true,
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,6 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
     name: String
     email: String
     password: String
