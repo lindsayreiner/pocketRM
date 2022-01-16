a7925f5af03e5ed8442944082306cb23de318d0e
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2022-01-16 00:12:11 -0500

    need to switch branches

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Button, Form, FormControl } from "react-bootstrap";
 // import Image from 'react-bootstrap/Image';
 
 
 export default function Contacts() {
     return (
         <>
-            <section>
+            <section className="contactList">
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
 
             </section>
         </>
