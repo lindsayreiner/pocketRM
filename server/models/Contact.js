5b79707788db10a8caca3cbf6fab09fe3ba6ce0f
tmantena1 <tmantena1@gmail.com>
2022-01-17 14:56:13 -0500

    incomplete models and resolver

--- server/models/Contact.js
+++ server/models/Contact.js
@@ -1,5 +1,9 @@
 const { Schema, model } = require("mongoose");
 
+//const noteSchema = require("./Notes");
+//const userSchema = require("./User");
+//const reminderSchema = require("./Reminder");
+
 const contactSchema = new Schema({
   firstName: {
     type: String,
@@ -106,6 +110,10 @@ const contactSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+
+  //savedNotes: [noteSchema],
+  //savedReminders: [reminderSchema]
+
 });
 
 const Contact = model("Contact", contactSchema);
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,64 @@
 const { User, Contact, Notes, Reminder } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return await User.find({}).populate("contacts", "reminders");
+      return await User.find({});
     },
-    contacts: async () => {
-      return await Contact.find({}).populate("notes", "reminders", "user");
+    contacts: async (parent, {userId}) => {
+      return Contact.find({_id: userId}).populate('contacts')
     },
-    notes: async () => {
-      return await Notes.find({}).populate("contacts");
+    notes: async (parent, {contactId}) => {
+     return Notes.find({_id: contactId}).populate("notes");
     },
-    reminders: async () => {
-      return await Reminder.find({}).populate("users", "contacts");
+    reminders: async (parent, {contactId}) => {
+     return Reminder.find({_id: contactId}).populate("reminders");
+    },
+
+  Mutation: {
+      addUser: async (parent, { firstName, lastName, email, password }) => {
+        const user = await User.create({ firstName, lastName, email, password });
+        const token = signToken(user);
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({email});
+        if (!user) {
+          throw new AuthenticationError("Incorrect email or password!");
+        }
+        const correctPassword = user.isCorrectPassword(password);
+        if (!correctPassword) {
+          throw new AuthenticationError("Wrong signon credentials");
+        }
+        const token = signToken(user);
+        return { token, user };
+      },
+      addContact: async (parent, args) => {
+        try {
+          const addToUserContact = await User.findOneAndUpdate(
+            { _id },
+            { $addToSet: { contacts: args } },
+            { new: true, runValidators: true }
+          );
+          return addToUserContact;
+        } catch (e) {
+          return `Unable to save contacts due to error: ${e}`;
+        }
+      },
+      deleteContact: async (parent, args, context) => {
+        try {
+          const removeFromUserContact = await User.findOneAndUpdate(
+            { _id: context.contact._id },
+            { $pull: { contacts: { _id: args._id } } },
+            { new: true, runValidators: true }
+          );
+          return removeFromUserContact;
+        } catch (e) {
+          return `Unable to delete contact due to error: ${e}`;
+        }
+      },
     },
   },
 };
