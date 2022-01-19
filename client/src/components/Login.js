064929b2d32a8574fabb747e9b3b054555006ddb
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-18 19:04:00 -0500

    updating AOS for login and register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
   return (
     <>
       <section className="loginCont container">
-        <Container data-aos="zoom-in" data-aos-delay="10">
+        <Container data-aos="fade-in">
           <h1 className="loginTitle">Log-in to your account</h1>
           <Form
             className="form"
@@ -39,32 +39,57 @@ export default function Login() {
               <Controller
                 name="email"
                 control={control}
-                render={({ field }) => <Form.Control {...field} {...register('email', { required: true, pattern: /^ (([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} />}
+                render={({ field }) => (
+                  <Form.Control
+                    {...field}
+                    {...register("email", {
+                      required: true,
+                      pattern:
+                        /^ (([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    })}
+                  />
+                )}
               />
             </Form.Group>
-            {(errors.email && errors.email.type === "required") && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Email field is required.</p>
+            {errors.email && errors.email.type === "required" && (
+              <p style={{ color: "red", marginBottom: "0.85rem" }}>
+                Email field is required.
+              </p>
             )}
-            {(errors.email && errors.email.type === "pattern") && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Enter a valid email address.</p>
+            {errors.email && errors.email.type === "pattern" && (
+              <p style={{ color: "red", marginBottom: "0.85rem" }}>
+                Enter a valid email address.
+              </p>
             )}
             <Form.Group>
               <Form.Label>Password</Form.Label>
               <Controller
                 name="password"
                 control={control}
-                render={({ field }) => <Form.Control type="password" {...field} {...register("password", { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/ })} />}
+                render={({ field }) => (
+                  <Form.Control
+                    type="password"
+                    {...field}
+                    {...register("password", {
+                      required: true,
+                      pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    })}
+                  />
+                )}
               />
             </Form.Group>
-            {(errors.password && errors.password.type === "required") && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Password field is required.</p>
+            {errors.password && errors.password.type === "required" && (
+              <p style={{ color: "red", marginBottom: "0.85rem" }}>
+                Password field is required.
+              </p>
             )}
-            {(errors.password && errors.password.type === "pattern") && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Passwords must be a minimum of 8 characters, and include at least one letter and one number.</p>
+            {errors.password && errors.password.type === "pattern" && (
+              <p style={{ color: "red", marginBottom: "0.85rem" }}>
+                Passwords must be a minimum of 8 characters, and include at
+                least one letter and one number.
+              </p>
             )}
-            <Button type="submit">
-              Login
-            </Button>
+            <Button type="submit">Login</Button>
           </Form>
 
           <div className="mt-3 d-flex justify-center">
