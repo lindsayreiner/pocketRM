29be992cc4e374e1387f26d47edb885d93c0480d
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-20 22:08:41 -0500

    reverting changes of login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-useless-escape */
-import { Button, Form, Container, Alert } from "react-bootstrap";
-import React, { useState } from "react";
+import React from "react";
+import { Button, Form, Container } from "react-bootstrap";
 import { useForm, Controller } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { loginUser } from "../utils/API";
-import Auth from "../utils/auth";
 // import Contacts from "./Contacts";
 
 export default function Login() {
   const {
     register,
+    handleSubmit,
     control,
     formState: { errors },
   } = useForm({
@@ -21,74 +20,32 @@ export default function Login() {
     },
   });
 
-  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    try {
-      const response = await loginUser(userFormData);
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-    setUserFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    });
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <>
       <section className="loginCont container">
-        <Container data-aos="fade-in">
-          <h1 className="loginTitle">Login to your account</h1>
+        <Container data-aos="zoom-in" data-aos-delay="10">
+          <h1 className="loginTitle">Log-in to your account</h1>
           <Form
             className="form"
-            onSubmit={handleFormSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             style={{ maxWidth: 450 }}
-            validated={validated}
           >
-            <Alert
-              dismissible
-              onClose={() => setShowAlert(false)}
-              show={showAlert}
-              variant="danger"
-            >
-              Something went wrong with your login credentials!
-            </Alert>
             <Form.Group>
               <Form.Label>Email Address</Form.Label>
               <Controller
                 name="email"
                 control={control}
-                // eslint-disable-next-line no-useless-escape
                 render={({ field }) => (
                   <Form.Control
-                    onChange={handleInputChange}
-                    value={userFormData.email}
                     {...field}
                     {...register("email", {
                       required: true,
-                      pattern: /^\S+@\S+\.\S+$/,
+                      pattern:
+                        /^ (([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\.[0-9]{1, 3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
                     })}
                   />
                 )}
@@ -99,11 +56,11 @@ export default function Login() {
                 Email field is required.
               </p>
             )}
-            {/* {errors.email && errors.email.type === "pattern" && (
+            {errors.email && errors.email.type === "pattern" && (
               <p style={{ color: "red", marginBottom: "0.85rem" }}>
                 Enter a valid email address.
               </p>
-            )} */}
+            )}
             <Form.Group>
               <Form.Label>Password</Form.Label>
               <Controller
@@ -111,8 +68,6 @@ export default function Login() {
                 control={control}
                 render={({ field }) => (
                   <Form.Control
-                    onChange={handleInputChange}
-                    value={userFormData.password}
                     type="password"
                     {...field}
                     {...register("password", {
@@ -138,8 +93,8 @@ export default function Login() {
           </Form>
 
           <div className="mt-3 d-flex justify-center">
-            New to PocketRM?
-            <Link to="/register" id="sign-up-link">
+            New to PocketRM?{" "}
+            <Link to="/register">
               <b>Sign Up</b>
             </Link>
           </div>
