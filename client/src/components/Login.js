da60b2545827c6987021cea56c6c0cc999c02d17
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-22 14:08:59 -0500

    fixed login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,55 @@
 /* eslint-disable no-useless-escape */
-import React from "react";
-import { Button, Form } from "react-bootstrap";
-import { useForm, Controller } from "react-hook-form";
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+// import { useForm, Controller } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 import "../styles/Login.css";
 
-export default function Login() {
-  const { register, handleSubmit, control, formState: { errors } } = useForm({
-    defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: ''
-    }
-  });
+const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    console.log(userFormData);
+
+    try {
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+
+      // Store the token in local storage
+      Auth.login(data.login.token);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      email: "",
+      password: "",
+    });
   };
 
   return (
@@ -26,57 +58,89 @@ export default function Login() {
         <h1 className="loginTitle">Login to your account</h1>
         <Form
           className="form"
-          onSubmit={handleSubmit(onSubmit)}
           style={{ maxWidth: 450 }}
+          noValidate
+          validated={validated}
+          onSubmit={handleFormSubmit}
         >
+          <Alert
+            dismissible
+            onClose={() => setShowAlert(false)}
+            show={showAlert}
+            variant="danger"
+          >
+            Something went wrong with your login credentials!
+          </Alert>
           <Form.Group>
-            <Form.Label>Email Address</Form.Label>
-            <Controller
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your email"
               name="email"
-              control={control}
-              render={({ field }) => <Form.Control {...field} {...register('email', { required: true, pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ })} />}
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+              // pattern: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
             />
+            <Form.Control.Feedback type="invalid">
+              Email is required!
+            </Form.Control.Feedback>
           </Form.Group>
-          {
-            (errors.email && errors.email.type === 'required') && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Email field is required.</p>
-            )
-          }
-          {
-            (errors.email && errors.email.type === 'pattern') && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Enter a valid email address.</p>
-            )
-          }
+          {/* {errors.email && errors.email.type === "required" && (
+            <p style={{ color: "red", marginBottom: "0.85rem" }}>
+              Email field is required.
+            </p>
+          )}
+          {errors.email && errors.email.type === "pattern" && (
+            <p style={{ color: "red", marginBottom: "0.85rem" }}>
+              Enter a valid email address.
+            </p>
+          )} */}
           <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Controller
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Your password"
               name="password"
-              control={control}
-              render={({ field }) => <Form.Control type="password" {...field} {...register('password', { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/ })} />}
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+              // pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
             />
+            <Form.Control.Feedback type="invalid">
+              Password is required!
+            </Form.Control.Feedback>
           </Form.Group>
-          {
-            (errors.password && errors.password.type === 'required') && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Password field is required.</p>
-            )
-          }
-          {
-            (errors.password && errors.password.type === 'pattern') && (
-              <p style={{ color: "red", marginBottom: "0.85rem" }}>Passwords must be a minimum of 8 characters, and include at least one letter and one number.</p>
-            )
-          }
-          <Button type="submit">
-            Login
+          {/* {errors.password && errors.password.type === "required" && (
+            <p style={{ color: "red", marginBottom: "0.85rem" }}>
+              Password field is required.
+            </p>
+          )}
+          {errors.password && errors.password.type === "pattern" && (
+            <p style={{ color: "red", marginBottom: "0.85rem" }}>
+              Passwords must be a minimum of 8 characters, and include at least
+              one letter and one number.
+            </p>
+          )} */}
+          <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type="submit"
+            className="btn-primary"
+            href="/dashboard"
+          >
+            Submit
           </Button>
-        </Form >
+        </Form>
 
         <div className="mt-3 d-flex justify-center">
           New to PocketRM?{" "}
-          <Link to="/register">
+          <Link to="/register" id="login-link">
             <b>Sign Up</b>
           </Link>
         </div>
-      </section >
+      </section>
     </>
   );
-}
+};
+
+export default LoginForm;
