d849ad5444a31cf55516b87e71c81fb017886e31
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-24 21:27:53 -0500

    adding context.id to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     user: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("contacts");
         return userData;
